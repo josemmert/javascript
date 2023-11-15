e88927279c2d0c0897dc70753aa500a9056a692f
jemmert <jozemel5@gmail.com>
2023-11-14 21:22:42 -0300

    se modifico el trabajo para hacerlo mas exacto con la primera parte de js

--- js/string.js
+++ js/string.js
@@ -45,3 +45,28 @@ console.log('El valor absoluto de num es: ', valorAbs);
 
 /* ARRAYS --> */ let arrayNumeros=[3,5,7,78,90,0, num];
 console.log('El contenido de array de numeros es: ', arrayNumeros);
+
+//max y min
+
+let max=Math.max(...arrayNumeros);
+console.log('El valor maximo del array de numeros es: ', max);
+
+let max2=Math.max(num,10,100,25);
+console.log('El valor maximo del array de numeros 2 es: ', max2);
+
+let min=Math.min(0,6,3,num);
+console.log('El valor minimo del array de numeros es: ', min);
+
+//El metodo Math.round() devuelve el valor de un numero redondeado al entero mas cercano. Por ejemplo:
+
+let numero2=5.5;
+let numeroRedondeado=Math.round(numero2)
+console.log('El valor del numero redondeado2 es: ', numeroRedondeado);
+
+let numero3=5.6;
+let numeroRedondeado2=Math.floor(numero3)
+console.log('El valor del numero redondeado2 es: ', numeroRedondeado2);
+
+let numero4=5.4;
+let numeroRedondeado3=Math.ceil(numero4)
+console.log('El valor del numero redondeado43 es: ', numeroRedondeado3);
