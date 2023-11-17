45540be67729dc4b1398b1a921fbef20fdf631a3
jemmert <jozemel5@gmail.com>
2023-11-16 21:27:45 -0300

    Se vieron los conceptos de los condicionales y arrays

---
+++ js/array.js
@@ -0,0 +1,94 @@
+console.log('Array');
+
+/**
+ * 
+ * let dias=[0,1,2,3,n]
+ */
+
+let dias=['lunes','martes','miercoles'];
+
+let longitud=dias.length;
+
+console.log('La longitud del array dias es: ', longitud);
+
+//acceder a los elementos de un array
+
+console.log('El valor de la posicion 0 del array es: ', dias[0]);
+console.log('El valor de la posicion 6 del array es: ', dias[6]); //Esto es una posicion que no existe
+console.log(dias.length);
+
+//agregar elementos a un array
+//array.push() agrega un nuevo elemento al final del array
+longitud=dias.push('jueves');
+longitud=dias.push('viernes')
+console.log('El valor de la longitud es:', longitud);
+console.log(dias);
+
+//array.unshift() agrega un nuevo elemento al inicio del array
+
+dias.unshift('Domingo');
+console.log('El array modificado es: ', dias);
+
+//agregar un elemento a un array en una posicion especifica--splice
+
+let array=[1,2,3];
+console.log('el array es-->', array);
+array.splice(1,0,5)
+console.log('el array es-->', array);
+
+//borrar elementos de un array
+
+//eliminar el ultimo elemento
+let arr=[1,2,3,4,5,6,7];
+console.log('el arr es--> ', arr);
+arr.pop()
+console.log('el arr es--> ', arr);
+
+//aliminar el primer elemento
+
+arr.shift();
+console.log('el arr es--> ', arr);
+
+//eliminar un elemento de cierta posicion de un array
+
+arr.splice(2,1);
+console.log('el arr es--> ', arr);
+
+//averiguar la posicion de un elemento
+let arrayNumeros=[1,2,3,4,5,6,7,8,9];
+let index=arrayNumeros.indexOf(5);
+arrayNumeros.splice(index,1)
+console.log('ArrayNumeros-->', arrayNumeros);
+
+//averiguar el valor maximo de un array de numeros
+
+let precios=[100,23,45,67,324,31,65]
+let valorMax=Math.max(...precios);
+console.log('el precio máximo es: ', valorMax);
+
+//averiguar el valor minimo de un array de numeros
+
+let valorMin=Math.min(...precios);
+console.log('El precio minimo es: ', valorMin);
+
+//ORDENAR ARRAYS
+
+let arrString=['a','z','x','c','h'];
+console.log('El array origina -->', arrString);
+
+let arrSortStr=arrString.sort();
+console.log('El array ORDENADO -->', arrSortStr);
+
+let arrNum=[2,10,54,3,6,9];
+console.log('El arrNum origina -->', arrNum);
+
+arrNum.sort((a,b)=> a - b);
+console.log('El arrNum ordenado -->', arrNum);
+
+//Filtrar array
+
+let arrOriginal=['perro','gato','conejo','elefante','rata'];
+console.log('Array original -->', arrOriginal);
+
+let arrFilter=arrOriginal.filter((element)=> element !== 'rata')
+console.log('Array filtrado es-->', arrFilter);
--- js/condicionales.js
+++ js/condicionales.js
@@ -8,6 +8,7 @@ console.log('Condicionales');
  */
 let edad=17;
 
+// IF BASICO
 // if(!(edad>18)){
 //     console.log('Eres mayor de edad');
 // }
@@ -18,6 +19,29 @@ let edad=17;
 //     console.log('Eres menor de 21 años');
 // }
 
+/** Definicion de pseudocodigo del IF
+ * 
+ * SI (CONDICION-A-EVALUAR=VERDADERO){
+ *      EJECUTA ESTA INSTRUCCION
+ * }
+ * 
+ * COMBINADO
+ * SI (CONDICION-A-EVALUAR=VERDADERO){
+ *      EJECUTA ESTA INSTRUCCION
+ * } SINO *SE EJECUTA EN CASO DE QUE LA CONDICION RESULTE FALSA*{
+ *      EJECUTA ESTA INSTRUCCION
+ * }
+ * SI (CONDICION-A-EVALUAR=VERDADERO){
+ *      EJECUTA ESTA INSTRUCCION
+ * } SINO SI (CONDICION-A-EVALUAR=VERDADERO){
+ *      EJECUTA ESTA INSTRUCCION
+ * } SINO{
+ *      EJECUTA ESTA INSTRUCCION
+ * }
+ */
+
+
+// IF COMBINADO
 if (edad<21) {
     console.log('Eres menor de edad, tu edad es:', edad);
 }else if (edad>=21 && edad<50) {
@@ -27,3 +51,151 @@ if (edad<21) {
 }else{
     console.log('Eres anciano, tu edad es: ', edad);
 }
+
+if (edad<21) {
+    if (edad>=18) {
+        console.log('Tienes entre 18 y 20 años');
+    } else {
+        console.log('Eres menor de 18 años');
+    }
+} else {
+    if (edad<50) {
+        console.log('Tienes entre 21 y 49 años');
+    } else {
+        console.log('Tienes 50 años o más');
+    }
+};
+
+/**CONDICIONALES MULTIPLES */
+
+/**
+ * EN CASO DE (EXPRESION A EVALUAR){
+ *      CASO: (COMPARA LA EXPRESION CON UN VALOR)
+ *      EJECUTA UNA INSTRUCCION
+ *      BREAK;
+ *      N...
+ *      DEFAULT
+ *      EJECUTA UNA INSTRUCCION
+ *      BREAK;
+ * }
+ */
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// let dia=parseInt(prompt('Por favor ingresar un numero'));
+let dia=5
+
+switch (dia) {
+    case 1:
+        console.log('Lunes');
+        document.write('Lunes')
+        break;
+    case 2:
+        console.log('Martes');
+        document.write('Martes')
+        break;
+    case 3:
+        console.log('Miercoles');
+        document.write('Miercoles')
+        break;
+    case 4:
+        console.log('Jueves');
+        document.write('Jueves')
+        break;
+    case 5:
+        console.log('Viernes');
+        document.write('Viernes')
+        break;
+    case 6:
+        console.log('Sabado');
+        document.write('Sabado')
+        break;
+    case 7:
+        console.log('Domingo');
+        document.write('Domingo')
+        break;
+
+    default:
+        console.log('No ingresaste un numero valido');
+        document.write('No ingresaste un numero valido')
+        break;
+};
+
+switch (dia) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        console.log('Es día laboral');
+        document.write('-Es dia laboral');
+        break;
+    case 6:
+    case 7:
+        console.log('Es fin de semana');
+        document.write('-Es fin de semana');
+        break;
+    default:
+        console.log('No es un dia valido');
+        break;
+}
+
+//BUCLES
+
+/**
+ * WHILE=MIENTRAS (CONDICION LOGICA)=TRUE{
+ *      EJECUTA LA INSTRUCCION
+ * }    
+ * 
+ */
+
+// while (condition) {
+    
+// }
+
+let cantidad=4;
+let contador=1;
+
+while (cantidad<=5) {
+    console.log('la cantidad es: ', cantidad);
+    cantidad++;
+    contador++;
+
+};
+console.log('El valor de contador es: ', contador);
+
+/**
+ *DO WHILE
+ */
+
+ do {
+    console.log('La cantidad es: ', cantidad);
+    cantidad++;
+    contador++;
+ } while (cantidad<=5);
+
+ console.log('el valor de contador es: ', contador);
+ console.log('el valor de cantidad es: ', cantidad);
+
+ //CICLOS
+/**
+ * FOR --> PARA
+ * PARA(INICIALIZACION; CONDICION; INCREMENTADOR){
+ *      INSTRUCCION A EJECUTAR
+ * }
+ */
+
+let contadorfor=0
+for (let index = 0; index < 10; index++) {
+    console.log('El valor de index es: ', index);
+    contadorfor++;
+}
+
+console.log('El ciclo for se ejecutó' + contadorfor + ' veces');
