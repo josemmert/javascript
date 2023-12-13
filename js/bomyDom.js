3f5bcbdb71b540f3e4536c116c570ee39d585aed
jemmert <jozemel5@gmail.com>
2023-12-12 21:27:30 -0300

    se vio BOM, eventos e hicimos un ejercicio con un cronometro

--- js/bomyDom.js
+++ js/bomyDom.js
@@ -4,30 +4,75 @@
 
 //  setInterval --> ejecuta una tarea cada un determinado lapso de tiempo
 
-const idInterval=setInterval(()=>console.log('Hola Mundo'), 1000);
+// const idInterval=setInterval(()=>console.log('Hola Mundo'), 1000);
 
-// clearInterval(idInterval) detiene la ejecucion del setInterval ue le pasamos su ID
+// // clearInterval(idInterval) detiene la ejecucion del setInterval ue le pasamos su ID
 
-//  setTimeout --> ejecuta una tarea una sola vez, pasado determinado tiempo
+// //  setTimeout --> ejecuta una tarea una sola vez, pasado determinado tiempo
 
-setTimeout(()=>{
-    console.log('detenemos el intervalo');
-    clearInterval(idInterval)
-}, 10000)
+// setTimeout(()=>{
+//     console.log('detenemos el intervalo');
+//     clearInterval(idInterval)
+// }, 10000)
 
 
-//! Cuenta Regresiva
+// //! Cuenta Regresiva
 
-let contador = 10;
+// let contador = 10;
 
-const cuentaRegresiva = setInterval(()=>{
-    console.log('Despegue en ', contador);
-    contador--;
+// const cuentaRegresiva = setInterval(()=>{
+//     console.log('Despegue en ', contador);
+//     contador--;
 
-},1000);
+// },1000);
 
-setTimeout(() => {
-    console.log('DESPEGUE!!');
-    clearInterval(cuentaRegresiva);
-}, 11000);
+// setTimeout(() => {
+//     console.log('DESPEGUE!!');
+//     clearInterval(cuentaRegresiva);
+// }, 11000);
 
+
+// DOM Document Object Model
+
+let body=document.body;
+console.log(body);
+
+body.innerHTML+="<h1 class='text-danger'>Hola Mundo</h1>";
+body.innerHTML+="<p class='bg-dark text-light'> lorem*5 </p>";
+
+//Pasandole el nombre de la etiqueta HTML
+/*let h2=document.querySelector("h2");
+console.log(h2);
+
+//Pasandole el ID
+
+let h2TCH=document.querySelector("#h2ToChange");
+console.log(h2TCH);
+*/
+
+let h2=document.getElementById('h2ToChange');
+console.log(h2);
+
+h2.textContent='H2 modificado';
+h2.classList.add('bg-secondary', 'text-dark');
+console.log(h2.classList);
+
+h2.classList.remove('bg-secondary');
+h2.classList.replace('text-dark','text-danger');
+
+let divPadre=document.getElementById('divPadre');
+
+console.log(divPadre);
+
+
+
+function insertarCard() {
+    divPadre.innerHTML+=`<div class="card" style="width: 18rem;">
+<img src="..." class="card-img-top" alt="...">
+<div class="card-body">
+  <h5 class="card-title">Card title</h5>
+  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <a href="#" class="btn btn-primary">Go somewhere</a>
+</div>
+</div>`;
+}
---
+++ js/cronometro.js
@@ -0,0 +1,76 @@
+console.log('Cronometro');
+let horas=0;
+let minutos=0;
+let segundos=0;
+let idInterval=undefined;
+
+let pantallaCronometro=document.getElementById('pantallaCronometro');
+console.log(pantallaCronometro);
+
+function Iniciar() {
+    console.log('desde iniciar');
+    if (idInterval===undefined) {
+        idInterval=window.setInterval(tiempo, 1);
+    }else{
+        console.log('id interval ya tiene un valor asignado');
+    }
+};
+
+function Pausar() {
+    window.clearInterval(idInterval);
+    
+};
+
+function Reiniciar() {
+    window.clearInterval(idInterval);
+    let idInterval=undefined;
+    horas=0;
+    minutos=0;
+    segundos=0;
+    pantallaCronometro.innerHTML=`0${horas}:0${minutos}:0${segundos}`;
+};
+
+function tiempo() {
+    let horaAux='';
+    let minutoAux='';
+    let segundoAux='';
+    segundos++;
+
+    if (segundos>59) {
+        segundos=0;
+        minutos++
+    };
+
+    if (minutos>59) {
+        minutos=0;
+        horas++;
+    };
+
+    if (horas>23) {
+        horas=0;
+        minutos=0;
+        segundos=0;
+    };
+
+    if (segundos<10) {
+        segundoAux=`0${segundos}`
+    }else {
+        segundoAux=`${segundos}`
+    };
+
+    if (minutos<10) {
+        minutoAux=`0${minutos}`
+    } else {
+        minutoAux=minutos;
+    };
+
+    if (horas<10) {
+        horaAux=`0${horas}`
+    } else {
+        horaAux=horas;
+    }
+
+
+
+    pantallaCronometro.innerHTML=`${horaAux}:${minutoAux}:${segundoAux}`;
+}
